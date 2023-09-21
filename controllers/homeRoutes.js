f281e14183766f20b69bda655b5a3ff7dc837cdd
jthomas47 <jackspurthomas@gmail.com>
2023-09-21 18:37:27 -0500

    created GET route to display all blog posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,33 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Blog, User } = require('../models');
 // TODO: create and require withAuth helper
+
+
+// Get route to view all posts
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('homepage', {
+            blogs
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
 
 
 module.exports = router;
