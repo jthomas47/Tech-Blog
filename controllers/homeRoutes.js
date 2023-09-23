55e799a99201f4e84013c260488e08e35fd2007e
jthomas47 <jackspurthomas@gmail.com>
2023-09-23 00:18:06 -0500

    added dashboard for creating blog posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,6 +26,25 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/dashboard', async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Blog }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
